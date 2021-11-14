96794276be817b9e0418455e9c61e381508640cc
Florian Pürschel <hello@prshl.de>
2021-11-14 15:51:46 +0100

    modify reader class and functions

--- src/reader.ts
+++ src/reader.ts
@@ -1,17 +1,24 @@
-import fs from "fs";
+import fs from 'fs';
 
 class Reader {
-  filetype: string;
-  path: string;
+	filetype: string;
+	path: string;
 
-  constructor(type: string) {
-    this.filetype = `.${type}`;
-    this.path = `./src/input/${type}`;
-  }
+	constructor(type: string) {
+		this.filetype = `.${type}`;
+		this.path = `./src/input/${type}`;
+	}
 
-  read(): string[] {
-    return fs.readdirSync(this.path);
-  }
+	readDirectory(): string[] {
+		return fs
+			.readdirSync(this.path)
+			.filter((filename) => !filename.toLowerCase().includes('ds_store'));
+	}
+
+	readFile(filename: string): string {
+		const file = fs.readFileSync(`${this.path}/${filename}`, 'utf-8');
+		return file;
+	}
 }
 
 export default Reader;

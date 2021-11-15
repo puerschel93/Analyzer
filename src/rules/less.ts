1434ac5773df5607a8248c23edb6025ef8f2df37
Florian Pürschel <hello@prshl.de>
2021-11-15 11:26:08 +0100

    add whole line identifier to rules in all pp

--- src/rules/less.ts
+++ src/rules/less.ts
@@ -17,19 +17,19 @@ import RuleSet from './rules';
  *   usage of the built-in Function version is tested in "module".
  */
 class Rules implements RuleSet {
-	extend: RegExp = new RegExp(/[^]+\:extend\([^]+\)\;/);
+	extend: RegExp = new RegExp(/^[^]+\:extend\([^]+\)\;/);
 	mixinWithoutArguments: RegExp = new RegExp(/(?!)/);
-  	mixinWithArguments: RegExp = new RegExp(/\.[^]+\(\@[a-zA-Z0-9\,\@ ]+\)[ ]*\{/);
+  	mixinWithArguments: RegExp = new RegExp(/^\.[^]+\(\@[a-zA-Z0-9\,\@ ]+\)[ ]*\{/);
 	function: RegExp = new RegExp(/(?!)/);
-	if: RegExp = new RegExp(/[^]+\(\@[^]+\)[ ]*when[ ]*\(\@[^]+\)[ ]*\{/);
+	if: RegExp = new RegExp(/^[^]+\(\@[^]+\)[ ]*when[ ]*\(\@[^]+\)[ ]*\{/);
 	else: RegExp = new RegExp(/(?!)/);
 	elseif: RegExp = new RegExp(/(?!)/);
-	each: RegExp = new RegExp(/[^]*each\([a-zA-Z0-9\@\_\-\.\{\}\(\)\, ]+[ ]*\{/);
+	each: RegExp = new RegExp(/^[^]*each\([a-zA-Z0-9\@\_\-\.\{\}\(\)\, ]+[ ]*\{/);
 	while: RegExp = new RegExp(/(?!)/);
 	for: RegExp = new RegExp(/(?!)/);
-	variable: RegExp = new RegExp(/\@[a-zA-Z0-9\-\_ ]+\:[ ]*[^]+\;/)
-	module: RegExp = new RegExp(/[^]*(if|list|escape|replace|length|extract|range|ceil|floor|percentage|round|sqrt|abs|append|sin|asin|cos|acos|tan|atan|pi|pow|mod|min|max|isnumber|isstring|iscolor|iskeyword|isurl|ispixel|isem|ispercentage|isunit|isruleset|isdefined|color|image-size|image-width|image-height|convert|data-uri|default|unit|get-unit|svg-gradient|hue|saturation|lightness|hsvhue|hsvsaturation|hsvvalue|red|green|blue|alpha|luma|luminance|saturate|desaturate|lighten|darken|fadein|fadeout|fade|spin|mix|tint|shade|greyscale|contrast|multiply|screen|overlay|softlight|hardlight|difference|exclusion|average|negation)+\([^]*\)[^]*/)
-	operator: RegExp = new RegExp(/[^]+[ ](\*|\+|\-|\%)[ ]{1,}[^]*/)
+	variable: RegExp = new RegExp(/^\@[a-zA-Z0-9\-\_ ]+\:[ ]*[^]+\;/)
+	module: RegExp = new RegExp(/^[^]*(if|list|escape|replace|length|extract|range|ceil|floor|percentage|round|sqrt|abs|append|sin|asin|cos|acos|tan|atan|pi|pow|mod|min|max|isnumber|isstring|iscolor|iskeyword|isurl|ispixel|isem|ispercentage|isunit|isruleset|isdefined|color|image-size|image-width|image-height|convert|data-uri|default|unit|get-unit|svg-gradient|hue|saturation|lightness|hsvhue|hsvsaturation|hsvvalue|red|green|blue|alpha|luma|luminance|saturate|desaturate|lighten|darken|fadein|fadeout|fade|spin|mix|tint|shade|greyscale|contrast|multiply|screen|overlay|softlight|hardlight|difference|exclusion|average|negation)+\([^]*\)[^]*/)
+	operator: RegExp = new RegExp(/^[^]+[ ](\*|\+|\-|\%)[ ]{1,}[^]*/)
 	array: RegExp[];
 
 	constructor() {
--- src/rules/scss.ts
+++ src/rules/scss.ts
@@ -1,19 +1,19 @@
 import RuleSet from './rules';
 
 class Rules implements RuleSet {
-	extend: RegExp = new RegExp(/@extend (\#*|\.*|\:{0,1})[a-zA-Z-0-9\[\]\']+\;*/);
-	mixinWithoutArguments: RegExp = new RegExp(/\@mixin[ ]+[a-zA-Z]+[ ]*\{/);
-  	mixinWithArguments: RegExp = new RegExp(/\@mixin[ ]+[a-zA-Z]+[ ]*\(\$[^]+\) \{/)
-	function: RegExp = new RegExp(/\@function[ ]+[a-zA-Z\-\_]+[ ]*\([^]*\) \{/);
-	if: RegExp = new RegExp(/\@if[ ]+[^]+[ ]*\{/)
-	else: RegExp = new RegExp(/\}[ ]*\@else[ ]*\{/)
-	elseif: RegExp = new RegExp(/\}[ ]*\@else if[ ]+[^]+[ ]*\{/)
-	each: RegExp = new RegExp(/\@each[ ]+\$[a-zA-Z0-9]+[ ]+in[ ]+[a-zA-Z0-9\$\,\(\)\.\% ]+[ ]*\{/)
-	while: RegExp = new RegExp(/\@while[ ]+[a-zA-Z0-9\$ \,\<\>\=\!]+[ ]*\{/)
-	for: RegExp = new RegExp(/\@for[ ]+\$[a-zA-Z0-9]+[ ]+from[ ]+[0-9]+[ ]+through[ ]+[0-9]+[ ]*\{/)
-	variable: RegExp = new RegExp(/\$[a-zA-Z0-9\-\_ ]+\:[ ]*[^]+\;/)
-	module: RegExp = new RegExp(/[^]*(adjust|adjust\-color|adjust\-hue|alpha|opacity|blackness|whiteness|red|blue|green|change|change\-color|complement|darken|desaturate|grayscale|saturation|lightness|lighten|hwb|ie\-hex\-str|invert|mix|opacify|fade\-in|saturate|transparentize|fade\-out|scale|append|index|is\-bracketed|join|length|list\-separator|separator|nth|set\-nth|slash|zip|map\-get|get|has\-key|map\-has\-key|keys|merge|map\-merge|map\-remove|remove|set|values|set\-values|ceil|clamp|floor|max|min|round|abs|hypot|log|pow|sqrt|cos|sin|tan|acos|asin|atan|atan2|compatible|comparable|is\-unitless|unitless|unit|div|percentage|random|load\-css|calc\-args|calc\-name|call|content\-exists|feature\-exists|function\-exists|get\-function|global\-variable\-exists|inspect|keywords|mixin\-exists|module\-functions|module\-variables|type\-of|is\-superselector|unquote|selector\-append|extend|selector\-extend|nest|selector\-nest|parse|selector\-parse|replace|selector\-replace|unify|selector\-unify|simple\-selectors|quote|index|str\-index|str\-insert|insert|length|str\-length|slice|str\-slice|to\-upper\-case|to\-lower\-case|unique\-id)+\(+[^]*/)
-	operator: RegExp = new RegExp(/[^]+[ ](\*|\+|\-|\%)[ ]{1,}[^]*/)
+	extend: RegExp = new RegExp(/^@extend (\#*|\.*|\:{0,1})[a-zA-Z-0-9\[\]\']+\;*/);
+	mixinWithoutArguments: RegExp = new RegExp(/^\@mixin[ ]+[a-zA-Z]+[ ]*\{/);
+  	mixinWithArguments: RegExp = new RegExp(/^\@mixin[ ]+[a-zA-Z]+[ ]*\(\$[^]+\) \{/)
+	function: RegExp = new RegExp(/^\@function[ ]+[a-zA-Z\-\_]+[ ]*\([^]*\) \{/);
+	if: RegExp = new RegExp(/^\@if[ ]+[^]+[ ]*\{/)
+	else: RegExp = new RegExp(/^\}[ ]*\@else[ ]*\{/)
+	elseif: RegExp = new RegExp(/^\}[ ]*\@else if[ ]+[^]+[ ]*\{/)
+	each: RegExp = new RegExp(/^\@each[ ]+\$[a-zA-Z0-9]+[ ]+in[ ]+[a-zA-Z0-9\$\,\(\)\.\% ]+[ ]*\{/)
+	while: RegExp = new RegExp(/^\@while[ ]+[a-zA-Z0-9\$ \,\<\>\=\!]+[ ]*\{/)
+	for: RegExp = new RegExp(/^\@for[ ]+\$[a-zA-Z0-9]+[ ]+from[ ]+[0-9]+[ ]+through[ ]+[0-9]+[ ]*\{/)
+	variable: RegExp = new RegExp(/^\$[a-zA-Z0-9\-\_ ]+\:[ ]*[^]+\;/)
+	module: RegExp = new RegExp(/^[^]*(adjust|adjust\-color|adjust\-hue|alpha|opacity|blackness|whiteness|red|blue|green|change|change\-color|complement|darken|desaturate|grayscale|saturation|lightness|lighten|hwb|ie\-hex\-str|invert|mix|opacify|fade\-in|saturate|transparentize|fade\-out|scale|append|index|is\-bracketed|join|length|list\-separator|separator|nth|set\-nth|slash|zip|map\-get|get|has\-key|map\-has\-key|keys|merge|map\-merge|map\-remove|remove|set|values|set\-values|ceil|clamp|floor|max|min|round|abs|hypot|log|pow|sqrt|cos|sin|tan|acos|asin|atan|atan2|compatible|comparable|is\-unitless|unitless|unit|div|percentage|random|load\-css|calc\-args|calc\-name|call|content\-exists|feature\-exists|function\-exists|get\-function|global\-variable\-exists|inspect|keywords|mixin\-exists|module\-functions|module\-variables|type\-of|is\-superselector|unquote|selector\-append|extend|selector\-extend|nest|selector\-nest|parse|selector\-parse|replace|selector\-replace|unify|selector\-unify|simple\-selectors|quote|index|str\-index|str\-insert|insert|length|str\-length|slice|str\-slice|to\-upper\-case|to\-lower\-case|unique\-id)+\(+[^]*/)
+	operator: RegExp = new RegExp(/^[^]+[ ](\*|\+|\-|\%)[ ]{1,}[^]*/)
 	array: RegExp[]
 
 	constructor() {

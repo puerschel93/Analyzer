9ed06418e868ce88c18962d96e6c23ecc82dc531
Florian Pürschel <hello@prshl.de>
2021-12-03 16:54:39 +0100

    implement argument call

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,51 @@
+/**
+ * Entrypoint for the analyzer.
+ * Created: 2021-11-28
+ * Author: Florian Pürschel
+ */
 import Analyzer from './analyzer';
 import Decloner from './decloner';
 import Logger from './utils/logger';
 
-const start = async () => {
+/**
+ * Runs a function depending on the given command.
+ */
+const start = (): void => {
+	const args = process.argv?.[0] ?? '';
+	switch (args) {
+		case 'analyze':
+			analyze();
+			break;
+		case 'declone':
+			declone();
+			break;
+		default:
+			Logger.error('Invalid command.\n');
+	}
+};
+
+/**
+ * Runs the analyzer on all files for all preprocessors.
+ * @returns {Promise<void>}
+ */
+const analyze = async (): Promise<void> => {
 	for (const analyzer of Analyzer) {
 		await analyzer.analyze();
 	}
 	Logger.success('All analyzers finished.\n');
+	return;
 };
 
-const decloner = async () => {
+/**
+ * Runs the decloner on all files for all preprocessors.
+ * @returns {Promise<void>}
+ */
+const declone = async (): Promise<void> => {
 	for (const decloner of Decloner) {
 		await decloner.declone();
 	}
+	return;
 };
 
-//start();
-decloner();
+/** Starts the program. */
+start();

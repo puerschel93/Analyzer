add5152d403b75cbb270c3af5c61b1b995e138dc
Florian Pürschel <hello@prshl.de>
2021-12-03 16:58:44 +0100

    add documentation to reader.ts

--- src/reader.ts
+++ src/reader.ts
@@ -1,5 +1,13 @@
+/**
+ * Created: 2021-11-27
+ * Author: Florian Pürschel
+ */
 import fs from 'fs';
 
+/**
+ * The reader class is used to read the content of a file or
+ * a directory.
+ */
 class Reader {
 	filetype: string;
 	path: string;
@@ -9,12 +17,21 @@ class Reader {
 		this.path = `./src/input/${type}`;
 	}
 
+	/**
+	 * Reads all files in a directory and returns an array of it.
+	 * @returns An array of all files in the directory
+	 */
 	readDirectory(): string[] {
 		return fs
 			.readdirSync(this.path)
 			.filter((filename) => !filename.toLowerCase().includes('ds_store'));
 	}
 
+	/**
+	 * This function reads the content of a file and returns it.
+	 * @param filename The name of the file to read
+	 * @returns the content of the file or false if the file does not exist
+	 */
 	readFile(filename: string): string | boolean {
 		try {
 			const file = fs.readFileSync(`${this.path}/${filename}`, 'utf-8');
